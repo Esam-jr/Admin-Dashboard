bda3f8f71da183542270bd811ee0edce244fe86b
Esmael <esmaelsabir9@gmail.com>
2024-10-30 14:10:42 +0300

    sidebar completed

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,77 @@
+import {
+  BarChart2,
+  DollarSign,
+  Menu,
+  Settings,
+  ShoppingBag,
+  ShoppingCart,
+  TrendingUp,
+  Users,
+} from "lucide-react";
+import { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import { Link } from "react-router-dom";
+
+const SIDEBAR_ITEMS = [
+  { name: "Overview", icon: BarChart2, color: "#6366f1", href: "/" },
+  { name: "Products", icon: ShoppingBag, color: "#8B5CF6", href: "/products" },
+  { name: "Users", icon: Users, color: "#EC4899", href: "/users" },
+  { name: "Sales", icon: DollarSign, color: "#10B981", href: "/sales" },
+  { name: "Orders", icon: ShoppingCart, color: "#F59E0B", href: "/orders" },
+  {
+    name: "Analytics",
+    icon: TrendingUp,
+    color: "#3B82F6",
+    href: "/analystics",
+  },
+  { name: "Settings", icon: Settings, color: "#6EE7B7", href: "/settings" },
+];
 function Sidebar() {
-  return <div></div>;
+  const [isSidebarOpen, setIssidebarOpen] = useState(true);
+  return (
+    <motion.div
+      className={`relative z-10 transition-all duration-300 ease-in-out flex-shrink-0 ${
+        isSidebarOpen ? "w-64" : "w-20"
+      }`}
+      animate={{ width: isSidebarOpen ? 256 : 80 }}
+    >
+      <div className="h-full bg-gray-800 bg-opacity-50 backdrop-blur-md flex flex-col p-4 border-r border-gray-700">
+        <motion.button
+          whileHover={{ scale: 1.1 }}
+          whileTop={{ scale: 0.9 }}
+          onClick={() => setIssidebarOpen(!isSidebarOpen)}
+          className="rounded-full p-2 hover:bg-gray-700 transition-colors max-w-fit"
+        >
+          <Menu size={24} />
+        </motion.button>
+        <nav className="mt-8 flex-grow">
+          {SIDEBAR_ITEMS.map((item) => (
+            <Link key={item.href} to={item.href}>
+              <motion.dev className="flex items-center p-4 text-sm font-medium rounded-lg mb-2 hover:bg-gray-700 transition-colors">
+                <item.icon
+                  size={20}
+                  style={{ color: item.color, minwidth: "20" }}
+                />
+                <AnimatePresence>
+                  {isSidebarOpen && (
+                    <motion.span
+                      className="ml-4 whitespace-nowrap text-white"
+                      initial={{ opacity: 0, width: 0 }}
+                      animate={{ opacity: 1, width: "auto" }}
+                      exit={{ opacity: 0, width: 0 }}
+                      transition={{ delay: 0.3, duration: 0.2 }}
+                    >
+                      {item.name}
+                    </motion.span>
+                  )}
+                </AnimatePresence>
+              </motion.dev>
+            </Link>
+          ))}
+        </nav>
+      </div>
+    </motion.div>
+  );
 }
 
 export default Sidebar;
